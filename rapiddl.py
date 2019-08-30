b4b82d970a8dc3fb42a99950b0e799f524c008bc
Christopher DiTusa <cditusa@gmail.com>
2019-08-30 11:26:05 -0400

    Version 1.3 -- see below for list of updates
    Add verify method to rename multi-part rar files.
    Prepend multiple links with index numbers in front of the file name.
    Remove .html from any links form within get function.
    Add fail fast parameter checking.

--- rapiddl.py
+++ rapiddl.py
@@ -1,23 +1,46 @@
 #!/usr/bin/env python3
 
-import threading, argparse, requests, os, rarfile, uuid, shutil, logging
+import threading, argparse, requests, os, rarfile, uuid, shutil, logging, pickle
 
 '''Downloads content from RapidGator.net for use with Plex Media server'''
 
-def get(session, link, path):
+def get(session, path, link, index=None):
+    '''Retrieves content from RapidGator.net
+    
+    Downloads a file from RapidGator. If there are multiple links
+    to download content from, we prepend an index number to the
+    filename so we can decompress the files in the correct order
+    later in the script.
+
+    Args:
+        session: A requests.Session object.
+        path: The destination path to download the file.
+        link: The url to download content from.
+        index: If the total number of links is greater than 1, prepend
+               the filename with an order number -- used later for 
+               decompressing multi-part rar files.
+               if the total number of links is equal to 1, omit
+               prepending the filename with a number
+    '''
+    if link[-5:] == '.html':
+        link = link[:-5]
     filename = link.split('/')[-1]
+    if index is not None:
+        filename = str(index) + filename
     with session.get(link, allow_redirects=True, stream=True) as response:
         with open(os.path.join(path, filename), 'wb') as file:
-            file.write(response.content)
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    file.write(chunk)
 
 def extract(file, path, video_formats):
     if file[-4:] == '.rar':
         with rarfile.RarFile(os.path.join(path, file)) as rf:
-            for file in rf.infolist():
-                if file.filename[-4:] in video_formats:
-                    rf.extract(file, path=path)
-                    video_file = os.path.join(path, file.filename)
-        return video_file
+            for rar_file in rf.infolist():
+                if rar_file.filename[-4:] in video_formats:
+                    rf.extract(rar_file, path=path)
+                    video_file = os.path.join(path, rar_file.filename)
+    return video_file
 
 def make_staging(path):
     staging_dir = os.path.split(path)[0]
@@ -26,10 +49,39 @@ def make_staging(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
+def verify(staging_path, files):
+    '''Verifies that the downloaded files are named appropriately.
+
+    When dealing with multi-part .rar files, the files need to have the same
+    base name in order to extract. If they do not, the extraction will fail. 
+    
+    This checks to see if the characters 1-5 of the first 2 files are the 
+    same. If they are, it returns the original list untouched. If they are 
+    different, it renames each one to 'download.part<n>.rar' where n is the 
+    part number.
+
+    The reason we use 1-5 and not 0-5 is becuase the index of the file
+    being downloaded is prepended to the file so that we can name them
+    appropriately in order to extract multi-part rar files in the correct order.
+
+    Args:
+        files: a list of files to check.
+
+    Returns:
+        A list of files.
+    '''
+    if files[0][1:5] != files[1][1:5]:
+        for i, file in enumerate(files):
+            os.rename(os.path.join(staging_path, file), 
+                        os.path.join(staging_path, 'download.part{}.rar'.format(i + 1)))
+        files = os.listdir(staging_path)
+    return files
+
 def main():
     parser = argparse.ArgumentParser(description='Rapidgator downloader')
     parser.add_argument('-l', '--link', required=True, nargs='+')
     parser.add_argument('-d', '--dest', required=True)
+    parser.add_argument('-n', '--filename')
     parser.add_argument('-u', '--username')
     parser.add_argument('-p', '--password')
     parser.add_argument('-v', '--verbose', action='count')
@@ -52,20 +104,32 @@ def main():
         'LoginForm[password]': args.password
     }
 
+    # Parameter checking (fail fast)
     if not os.path.exists(args.dest):
         raise FileNotFoundError(args.dest, ' does not exist')
+    if not args.username or not args.password:
+        logger.info('Credentials not supplied -- searching for auth file')
+        if os.path.exists('auth'):
+            with open('auth', 'rb') as auth_file:
+                LOGIN_PAYLOAD = pickle.load(auth_file)
+                logger.info('Authentication file imported')
+        else:
+            raise FileNotFoundError('Authentication file not found')
 
     threads = []
     video_formats = ['.mkv','.mp4']
+    zip_formats = ['.rar']
     staging_path = os.path.join(os.getcwd(), 'staging', str(uuid.uuid4()))
     make_staging(staging_path)
     logger.info('Staging path created')
     with requests.Session() as session:
-        session = requests.Session()
         post = session.post(LOGIN_URL, data=LOGIN_PAYLOAD)
         logger.info('Authentication successful')
-        for link in args.link:
-            thread = threading.Thread(target=get, args=(session, link, staging_path))
+        for index, link in enumerate(args.link):
+            if len(args.link) == 1:
+                index = None
+            thread = threading.Thread(target=get, 
+                args=(session, staging_path, link, index))
             threads.append(thread)
             thread.start()
             logger.info('Downloading file from {}'.format(link))
@@ -74,21 +138,21 @@ def main():
         logger.info('Downloading complete')
     files = os.listdir(staging_path)
     files.sort()
-    # Rename any files ending in '.html'
-    if files[0][-5:] == '.html':
-        for file in files:
-            os.rename(os.path.join(staging_path, file),
-                        os.path.join(staging_path, file[:-5]))
-        files = os.listdir(staging_path)
-        files.sort()
+    if len(files) > 1:
+        files = verify(staging_path, os.listdir(staging_path))
     # Extract archives
-    if files[0][-4:] in ['.rar']:
+    if files[0][-4:] in zip_formats:
         file = extract(files[0], staging_path, video_formats)
         logger.info('File extraction complete')
     # Move files that do not need to be decompressed
     elif files[0][-4:] in video_formats:
         file = os.path.join(staging_path, files[0])
-    shutil.move(file, args.dest)
+    if args.filename:
+        filename = "{}.{}".format(args.filename, file[-3:])
+        logger.info('File renamed')
+        shutil.move(file, os.path.join(args.dest, filename))
+    else:
+        shutil.move(file, args.dest)
     logger.info('File moved successfully')
     shutil.rmtree(staging_path)
     logger.info('Staging folder removed')
