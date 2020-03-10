a78f43de5afbc72048b7dc2ae9c0d2d3535d8809
Christopher DiTusa <cditusa@gmail.com>
2020-03-10 12:18:36 -0400

    Convert rapiddl.py into class objects

--- rapiddl/__init__.py
+++ rapiddl/__init__.py
@@ -17,7 +17,6 @@ class Downloader():
     """
     def __init__(self, file_handler, token=None):
         self.token = token
-        self.auth_url = None
         self.file_handler = file_handler
         if self.token is None:
             raise ValueError("token parameter must be supplied.")
@@ -25,13 +24,6 @@ class Downloader():
     def __str__(self):
         return u'\n\t'.format(self.links) + '\n\t'.join([u'{}:{}'.format(k, v)
                                                          for k, v in self.__dict__.items() if v])
-
-    def _get_links(self):
-        '''Returns a list of links'''
-        return self.links
-
-    def _check_download_status(self):
-        pass
 
     def get(self, uri, token):
         """Authenticate and download files"""
@@ -48,12 +40,12 @@ class Rapidgator(Downloader):
                      remote server
     """
     def __init__(self, file_handler, token=None):
-        self.auth_url = 'https://rapidgator.net/auth/login'
         self.token = token
-        self.threads = []
         self.file_handler = file_handler
+        self.auth_url = 'https://rapidgator.net/auth/login'
+        self.threads = []
         super().__init__(self.file_handler, self.token)
-
+        
     def _get(self, link, session):
         """Worker function called inside each thread.
     
@@ -61,13 +53,26 @@ class Rapidgator(Downloader):
             link (str): the link to download.
             session (requests.Session): a session object.
         """
-        file_name = link.split('/')[-1]
+        file_name = self._clean_file_name(link.split('/')[-1])
         with session.get(link, allow_redirects=True, stream=True) as response:
             with open(os.path.join(
                 self.file_handler.staging_path, file_name), 'wb') as file:
                     for chunk in response.iter_content(chunk_size=1024):
                         if chunk:
                             file.write(chunk)
+
+    def _clean_file_name(self, file_name):
+        """Removes .html from the end of the file name, if necessary.
+        
+        Args:
+            file_name (str): the filename to clean.
+
+        Return:
+            str: a cleaned filename.
+        """
+        if '.html' in file_name:
+           return file_name[:-5]
+        return file_name
 
     def get(self, link, token=None):
         """Downloads a file from RapidGator.
@@ -84,16 +89,15 @@ class Rapidgator(Downloader):
             token = self.token
         with requests.Session() as session:
             response = session.post(self.auth_url, token)
-            for link in links:
-                thread = threading.Thread(
-                    target=_get, args=(link, ))
-                self.threads.append(thread)
-                thread.start()
+            thread = threading.Thread(
+                target=self._get, args=(link, session))
+            self.threads.append(thread)
+            thread.start()
 
     def wait(self):
         """Wait for all threads to finish processesing"""
-        for thead in self.threads:
-            thread.wait()
+        for thread in self.threads:
+            thread.join()
 
 
 class FileHandler():
@@ -104,22 +108,26 @@ class FileHandler():
         file_extractor (obj): Object capable of extracting files.
     """
     def __init__(self, path, file_extracter):
-        self.file_extracter  = file_extracter
         self.path = path
+        self.file_extracter  = file_extracter
         self.staging_path = self.make_staging()
 
-    def make_staging(self):
+    def make_staging(self, path=None):
         """Creates the staging path.
         Uses uuid4 to create a unique path in the destination directory
         which is used as a staging area for file download/extraction.
         
         Return:
-            str: Staging directory path.
+            str: staging directory path.
         """
-        if os.path.exists(self.path):
-            staging_path = os.path.join(self.path, str(uuid.uuid4()))
-            os.mkdir(staging_path)
-            return staging_path
+        if not os.path.exists(self.path):
+            raise FileNotFoundError("Destination path does not exist.")
+        if path is None:
+            path = self.path
+        staging_path = os.path.join(self.path, str(uuid.uuid4()))
+        os.mkdir(staging_path)
+        return staging_path
+        
 
     def remove_staging(self):
         """Removes the staging path"""
@@ -132,14 +140,19 @@ class FileExtracter():
 
 def main():
     token = {'LoginForm[email]': 'metsfan2152@gmail.com',
-             'LoginForm[password]': 'Wogman2152'}
-    destination_path = 'c:\\temp\\dl'
+             'LoginForm[password]': 'NotThePassword'}
+    destination_path = '/tmp/staging'
     link = 'https://rapidgator.net/file/21f84529c90684a59903b7cd9d8a72b1/Spaceballs.1987.1080p.BluRay.x264.YIFY.mp4.html'
 
     fh = FileHandler(destination_path, None)
     rg = Rapidgator(fh, token)
     rg.get(link)
     rg.wait()
+
+
+
+    # clean up 
+    rg.file_handler.remove_staging()
 
 if __name__ == '__main__':
     main()
--- test_rapiddl.py
+++ test_rapiddl.py
@@ -28,36 +28,59 @@ class TestRapidgator(unittest.TestCase):
 
     @mock.patch('rapiddl.Rapidgator.wait')
     def test_wait_method_calls_thread_wait(self, mock_wait):
-        """"""
+        """wait() method calls Thread.wait()"""
         self.rg.wait()
         mock_wait.assert_called_once()
     
     @mock.patch('rapiddl.open')
     def test__get_method_calls_session_get(self, mock_open):
-        """_get method should call session.get twice"""
+        """_get() method should call session.get twice"""
         mock_session = mock.create_autospec(requests.Session)
         for link in self.links:
             self.rg._get(link, mock_session)
         self.assertEqual(mock_session.get.call_count, 2)
         self.assertEqual(mock_open.call_count, 2)
 
+    def test_clean_file_name_returns_expected_result(self):
+        """_clean_file_name() properly cleans the file_name"""
+        file1 = 'Spaceballs.1987.1080p.BluRay.x264.YIFY.mp4.html'
+        result1 = self.rg._clean_file_name(file1)
+        self.assertEqual(result1, 'Spaceballs.1987.1080p.BluRay.x264.YIFY.mp4')
+        
+        file2 = 'Spaceballs.1987.1080p.BluRay.x264.YIFY.mp4'
+        result2 = self.rg._clean_file_name(file2)
+        self.assertEqual(result2, 'Spaceballs.1987.1080p.BluRay.x264.YIFY.mp4')
+
 
 class TestFileHandler(unittest.TestCase):
-    def setUp(self):
-        self.file_handler = FileHandler('/tmp', None)
-
-    @mock.patch('rapiddl.os.path.exists', return_value=True)
     @mock.patch('rapiddl.os.mkdir')
+    def setUp(self, mock_mkdir):
+        self.file_handler = FileHandler('/tmp/staging', None)
+
+    @mock.patch('rapiddl.os.mkdir')
+    @mock.patch('rapiddl.os.path.exists', return_value=True)
     @mock.patch('rapiddl.uuid.uuid4', return_value='b75bc907-c056-4e82-b015-c9851075721e')
-    def test_make_staging(self, mock_uuid4, mock_mkdir, mock_exists):
+    def test_make_staging(self, mock_uuid4, mock_exists, mock_mkdir):
         self.file_handler.make_staging()
         mock_uuid4.assert_called_once()
-        mock_mkdir.assert_called_once()
+        mock_mkdir.assert_called_with('/tmp/staging/b75bc907-c056-4e82-b015-c9851075721e')
 
     @mock.patch('rapiddl.os.rmdir')
     def test_remove_staging(self, mock_rmdir):
         self.file_handler.remove_staging()
         mock_rmdir.assert_called_once()
+
+    @mock.patch('rapiddl.os.path.exists', return_value=True)
+    def test_destination_exists(self, mock_exists):
+        """Tests if the destination path exists."""
+        pass
+        
+    @mock.patch('rapiddl.os.path.exists', return_value=False)
+    def test_destination_does_not_exist(self, mock_exists):
+        """Tests if the destination path does not exists."""
+        path = '/does_not_exist'
+        with self.assertRaises(FileNotFoundError) as exception:
+            self.file_handler.make_staging(path).should_raise()
 
 
 if __name__ == '__main__':
