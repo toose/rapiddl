703788703ac3c4431e6e29136191873afdcbb9a5
Christopher DiTusa <cditusa@gmail.com>
2020-04-10 09:31:38 -0400

    Revert class object, favor function
    - Built out helper functions: parse_args, get, _get, unzip, main,
      clean_name, build_payload, make_staging
    - Designed unittest tests for each function, with 88% code coverage

--- rapiddl/__init__.py
+++ rapiddl/__init__.py
@@ -1,169 +1,181 @@
-#!/usr/bin/env python3
-
-import threading
+#!/usr/sbin/env python3
 import argparse
 import os
-import rarfile
-import uuid
-import logging
+import sys
 import shutil
-import pickle
+import logging
+import uuid
+import re
+import threading
 import requests
+import rarfile
 
 
-class Downloader():
-    """Base object. Inherit to make subclasses for other file hosting 
-    websites
-    """
-    def __init__(self, file_handler, token=None):
-        self.token = token
-        self.file_handler = file_handler
-        self.threads = []
-        if self.token is None:
-            raise ValueError("token parameter must be supplied.")
+logger = logging.getLogger('rapiddl.main')
 
-    def __str__(self):
-        return u'\n\t'.format(self.links) + '\n\t'.join([u'{}:{}'.format(k, v)
-                                                         for k, v in self.__dict__.items() if v])
-
-    def get(self, uri, token):
-        """Authenticate and download files"""
-        pass
-
-
-class Rapidgator(Downloader):
-    """An object which encapculates the process of authenticating against
-    rapidgator.net as well as downloading one or more files.
+def clean_name(link):
+    '''Cleans a file name, stripping it it's base url.
     
     Args:
-        links (list): a list of links.
-        token (str): token information needed to authenticate to the
-                     remote server
-    """
-    def __init__(self, file_handler, token=None):
-        self.auth_url = 'https://rapidgator.net/auth/login'
-        super().__init__(self.file_handler, self.token)
-        
-    def _get(self, link, session):
-        """Worker function called inside each thread.
+        link (str): a link/url to clean
+
+    Returns:
+        (str): a suitable file name
+    '''
+    if link[-5:] == '.html':
+        link = link[:-5]
+    file_name = link.split('/')[-1]
+    logger.info(f' file name: {file_name}')
+    return file_name
+
+def build_payload(username, password):
+    '''Returns authentication information in the form of dict.
+
+    Args:
+        username (str): the username to authenticate with
+        password (str): the password to authenticate with
+
+    Returns:
+        (dict): login payload
+    '''
+
+    if not re.match(r'[^@]+@[^@]+\.[^@]+', username):
+        raise ValueError('Username is not a valid email address.')
+    return {'LoginForm[email]': username,
+            'LoginForm[password]': password}
+
+def make_staging(destination):
+    '''Creates a staging directory based off uuid.uuid4[:8] and returns
+    the path as a string.
     
-        Args:
-            link (str): the link to download.
-            session (requests.Session): a session object.
-        """
-        file_name = self._clean_file_name(link.split('/')[-1])
-        with session.get(link, allow_redirects=True, stream=True) as response:
-            with open(os.path.join(
-                self.file_handler.staging_path, file_name), 'wb') as file:
-                    for chunk in response.iter_content(chunk_size=1024):
-                        if chunk:
-                            file.write(chunk)
+    Args:
+        destination (str): the original destination path.
+    Returns:
+        (str): staging path
+    '''
+    guid = str(uuid.uuid4())[:8]
+    staging_path = os.path.join(destination, guid)
+    os.mkdir(staging_path)
+    logger.info(f' staging path: {staging_path}')
+    return staging_path
 
-    def _clean_file_name(self, file_name):
-        """Removes .html from the end of the file name, if necessary.
-        
-        Args:
-            file_name (str): the filename to clean.
+def parse_args(args):
+    '''Parses arguemnts using the argparse package.
 
-        Return:
-            str: a cleaned filename.
-        """
-        if '.html' in file_name:
-           return file_name[:-5]
-        return file_name
+    Args:
+        args (list): arguments passed from sys.argv
 
-    def get(self, link, token=None):
-        """Downloads a file from RapidGator.
+    Returns:
+        argparse.NameSpace
+    '''
+    parser = argparse.ArgumentParser(description='Rapidgator downloader')
+    parser.add_argument('-l', '--link', nargs='+')
+    parser.add_argument('-d', '--destination', required=True)
+    parser.add_argument('-f', '--file')
+    parser.add_argument('-e', '--extract', nargs='?', default=False, const=True)
+    parser.add_argument('-n', '--filename')
+    parser.add_argument('-u', '--username')
+    parser.add_argument('-p', '--password')
+    parser.add_argument('-v', '--verbose', action='count')
+    return parser.parse_args(args)
 
-        Args:
-            link (str): a rapidgator url.
-            token (str): used to authenticate the get method with
-                rapidgator.
+def get(session, link, path, threads):
+    '''Uses thread objects to concurrently download the remote file(s).
+    
+    Args:
+        session (requests.Session): a valid session object
+        link (str): a valid rapidgator link
+        path (str): the destination path to download the file(s)
+        threads (list): threading.Thread list
+    '''
+    thread = threading.Thread(target=_get, args=(session, link, path))
+    threads.append(thread)
+    thread.start()
+    
 
-        Returns:
-            session: requests.Session object.
-        """
-        if token is None:
-            token = self.token
-        with requests.Session() as session:
-            response = session.post(self.auth_url, token)
-            thread = threading.Thread(
-                target=self._get, args=(link, session))
-            self.threads.append(thread)
-            thread.start()
+def _get(session, link, path):
+    '''Encapsulates the process of fetching and saving the file(s)
+    to disk.
 
-    def wait(self):
-        """Wait for all threads to finish processesing"""
-        for thread in self.threads:
+    Args:
+        session (requests.Session): a valid session object
+        link (str): a valid rapidgator link
+        path (str): the destination path to download the file(s)
+    '''
+    file_name = clean_name(link)
+    logger.info(f' downloading file from: {link}')
+    with session.get(link, allow_redirects=True, stream=True) as response:
+        with open(os.path.join(path, file_name), 'wb') as file:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    file.write(chunk)
+
+def unzip(file, path):
+    '''Unzips files with the .rar extension
+
+    Args:
+        file: full path to the file that you want to extract
+        path (str): full path to the zip file
+    '''
+    file = os.path.join(path, file)
+    file_name = file.split(os.path.sep)[-1]
+    try:
+        with rarfile.RarFile(file, crc_check=False) as rf:
+            for rar_file in rf.infolist():
+                rf.extract(rar_file, path=path)
+                logging.info(f' extracting {file_name} to {path}')
+    except Exception as err:
+        logger.warning(str(err))
+
+
+def main(args):
+    parser = parse_args(args)
+
+    if parser.verbose == None:
+        log_level = logging.ERROR
+    elif parser.verbose == 1:
+        log_level = logging.WARN
+    elif parser.verbose == 2:
+        log_level = logging.INFO
+    elif parser.verbose > 2:
+        log_level = logging.DEBUG
+    logging.basicConfig(level=log_level)
+
+    if not os.path.exists(parser.destination):
+        raise FileNotFoundError(f'{parser.destination} does not exist.')
+    else:
+        staging_path = make_staging(parser.destination)
+
+    threads = []
+    ZIP_FORMATS = ['.rar']
+    VIDEO_FORMATS = ['.mkv','.mp4']
+    LOGIN_URL = 'https://rapidgator.net/auth/login'
+    LOGIN_PAYLOAD = build_payload(parser.username,
+                                  parser.password)
+
+    with requests.Session() as session:
+        post = session.post(LOGIN_URL, data=LOGIN_PAYLOAD)
+        for link in parser.link:
+            get(session, link, staging_path, threads)
+        for thread in threads:
             thread.join()
+    for file in os.listdir(staging_path):
+        full_path = os.path.join(staging_path, file)
+        if file[-4:] in ZIP_FORMATS:
+            if parser.extract:
+                logger.info(f' extracting files to {parser.destination}')
+                unzip(file=full_path, path=parser.destination)
+            else:
+                shutil.move(full_path, parser.destination)
+        else:
+            try:
+                shutil.move(full_path, parser.destination)
+                logger.info(f' {file} moved to {parser.destination}')
+            except Exception as err:
+                logger.error(str(err))
+    shutil.rmtree(staging_path)
+    logger.info(f' removing staging directory: {staging_path}')
 
-
-class FileHandler():
-    """A file handler object to saving files to disk.
-    
-    Args:
-        path (str): the destination path to save the file.
-        file_extractor (obj): object capable of extracting files.
-    """
-    def __init__(self, path, file_extractor):
-        self.path = path
-        self.file_extractor = file_extractor
-        self.staging_path = self.make_staging()
-
-    def is_compressed(self, file):
-        compression_formats = ['.rar.', '.zip', '.7z']
-        if file[-4:] in compression_formats:
-            return True
-        return False
-
-
-    def make_staging(self, path=None):
-        """Creates the staging path.
-        Uses uuid4 to create a unique path in the destination directory
-        which is used as a staging area for file download/extraction.
-        
-        Return:
-            str: staging directory path.
-        """
-        if not os.path.exists(self.path):
-            raise FileNotFoundError("Destination path does not exist.")
-        if path is None:
-            path = self.path
-        staging_path = os.path.join(self.path, str(uuid.uuid4()))
-        os.mkdir(staging_path)
-        return staging_path
-        
-
-    def remove_staging(self):
-        """Removes the staging path"""
-        os.rmdir(self.staging_path)
-
-
-class FileExtracter():
-    def __init__():
-        pass
-
-    def extract():
-        pass
-
-
-def main():
-    token = {'LoginForm[email]': 'metsfan2152@gmail.com',
-             'LoginForm[password]': 'Wogman2152'}
-    destination_path = '/tmp/staging'
-    link = 'https://rapidgator.net/file/21f84529c90684a59903b7cd9d8a72b1/Spaceballs.1987.1080p.BluRay.x264.YIFY.mp4.html'
-
-    fh = FileHandler(destination_path, None)
-    rg = Rapidgator(fh, token)
-    rg.get(link)
-    rg.wait()
-    
-    for file in os.listdir(rg.file_handler.staging_path):
-        if rg.file_handler.is_compressed(file):
-            rg.file_handler
-
-    # clean up
-    rg.file_handler.remove_staging()
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
--- test_rapiddl.py
+++ test_rapiddl.py
@@ -1,97 +1,109 @@
-import unittest, requests
-from unittest import mock
-from rapiddl import Rapidgator, FileHandler
-from tempfile import TemporaryDirectory, TemporaryFile
+from unittest import TestCase, mock
+from tempfile import TemporaryDirectory, gettempdir
+import os
+import logging
+import requests
+import uuid
+
+from rapiddl import main, clean_name, build_payload, make_staging, get, _get, unzip
 
 
-class TestRapidgator(unittest.TestCase):
-    def setUp(self):
-        self.token = {'LoginForm[email]': 'name@email.com', 
-                      'LoginForm[password]': 'TestPass123'}
-        self.links = ['https://example.com/some-file.part1.rar',
-                      'https://example.com/some-file.part2.rar']
-        
-        self.mock_file_handler = mock.create_autospec(FileHandler)
-        self.mock_file_handler.staging_path = '/tmp/staging/'
-        self.rg = Rapidgator(self.mock_file_handler, token=self.token)
-
-    def test_no_token_raises_exception(self):
-        """Raises an exception when no token is provided"""
-        self.assertRaises(ValueError, Rapidgator, self.mock_file_handler)
-
-    @mock.patch('rapiddl.Rapidgator.get')
-    def test_get_method_executes_twice(self, mock_get):
-        """Confirm methods in get function execte X times"""
-        for link in self.links:
-            self.rg.get(link)
-            mock_get.assert_called_with(link)
-
-    @mock.patch('rapiddl.Rapidgator.wait')
-    def test_wait_method_calls_thread_wait(self, mock_wait):
-        """wait() method calls Thread.wait()"""
-        self.rg.wait()
-        mock_wait.assert_called_once()
-    
-    @mock.patch('rapiddl.open')
-    def test__get_method_calls_session_get(self, mock_open):
-        """_get() method should call session.get twice"""
-        mock_session = mock.create_autospec(requests.Session)
-        for link in self.links:
-            self.rg._get(link, mock_session)
-        self.assertEqual(mock_session.get.call_count, 2)
-        self.assertEqual(mock_open.call_count, 2)
-
-    def test_clean_file_name_returns_expected_result(self):
-        """_clean_file_name() properly cleans the file_name"""
-        file1 = 'Spaceballs.1987.1080p.BluRay.x264.YIFY.mp4.html'
-        result1 = self.rg._clean_file_name(file1)
-        self.assertEqual(result1, 'Spaceballs.1987.1080p.BluRay.x264.YIFY.mp4')
-        
-        file2 = 'Spaceballs.1987.1080p.BluRay.x264.YIFY.mp4'
-        result2 = self.rg._clean_file_name(file2)
-        self.assertEqual(result2, 'Spaceballs.1987.1080p.BluRay.x264.YIFY.mp4')
-
-
-class TestFileHandler(unittest.TestCase):
+class TestRapiddl(TestCase):
     @mock.patch('rapiddl.os.mkdir')
     def setUp(self, mock_mkdir):
-        self.file_handler = FileHandler('/tmp/staging', None)
+        self.tempdir = '/tmp/'
+        self.threads = []
+        self.staging_path = make_staging(gettempdir())
+        self.link = 'https://rapidgator.net/file/fc99592c0f8b17f32cf2b8258b1eb5e0/Up.in.Smoke.1978.720p.BRRip.H264.AAC-RARBG.rar.html'
+        self.session = mock_session = mock.create_autospec(requests.Session)
+
+    @mock.patch('rapiddl.os.path.exists', return_value=False)
+    def test_destination_exists(self, mock_exists):
+        '''Raises an exception when the destination does not exist'''
+        with self.assertRaises(FileNotFoundError) as err:
+            main(['-l','https//home.net', '-d', '/DoesNotExist', '-v'])
+        mock_exists.assert_called_with('/DoesNotExist')
+
+    def test_clean_name(self):
+        name = clean_name(self.link)
+        self.assertEqual(name, 'Up.in.Smoke.1978.720p.BRRip.H264.AAC-RARBG.rar')
+
+    def test_build_payload(self):
+        '''Checks username/password format'''
+        payload = build_payload('user@email.com', 'testpass123')
+        self.assertEqual(payload['LoginForm[email]'], 'user@email.com')
+        self.assertEqual(payload['LoginForm[password]'], 'testpass123')
+
+    def test_invalid_username(self):
+        '''Checks that the username is an email address'''
+        with self.assertRaises(ValueError) as err:
+            build_payload('invalid_user', 'testpass123')
 
     @mock.patch('rapiddl.os.mkdir')
+    def test_make_staging(self, mock_mkdir):
+        '''Tests staging folder creation'''
+        staging_path = make_staging(gettempdir())
+        mock_mkdir.assert_called_with(staging_path)
+
+    @mock.patch('rapiddl.os.mkdir')
+    @mock.patch('rapiddl.threading.Thread')
+    def test_get_method(self, mock_thread, mock_mkdir):
+        '''Get method calls new Thread'''
+        path = os.path.join(gettempdir(), str(uuid.uuid4())[:8])
+        get(self.session, self.link, path, self.threads)
+        mock_thread.assert_called_once()
+
+    @mock.patch('rapiddl.open')
+    def test__get_method(self, mock_open):
+        '''Tests writing remote file to disk'''
+        _get(self.session, self.link, self.staging_path)
+        mock_open.assert_called()
+
+    @mock.patch('rapiddl.rarfile.RarFile', auto_spec=True)
+    def test_unzip_method(self, mock_rarfile):
+        '''Unzips the contents of a .rar file'''
+        mock_rarfile.return_value.__enter__.return_value.infolist.return_value = ['file1.mp4']
+        file = '/path/to/archive.rar'
+        destination = '/tmp/'
+        unzip(file=file, path=destination)
+
+        mock_rarfile.assert_called()
+        mock_rarfile.return_value.__enter__.return_value.extract.assert_called_with('file1.mp4', path='/tmp/')
+
+    @mock.patch('rapiddl.shutil.rmtree')
+    @mock.patch('rapiddl.os.listdir', return_value=['archive.rar'])
     @mock.patch('rapiddl.os.path.exists', return_value=True)
-    @mock.patch('rapiddl.uuid.uuid4', return_value='b75bc907-c056-4e82-b015-c9851075721e')
-    def test_make_staging(self, mock_uuid4, mock_exists, mock_mkdir):
-        self.file_handler.make_staging()
-        mock_uuid4.assert_called_once()
-        mock_mkdir.assert_called_with('/tmp/staging/b75bc907-c056-4e82-b015-c9851075721e')
+    @mock.patch('rapiddl.make_staging', return_value='/tmp/438cab0f')
+    @mock.patch('rapiddl.unzip')
+    @mock.patch('rapiddl.get')
+    @mock.patch('rapiddl.shutil.move')
+    def test_main_method_rar_without_extraction(self, mock_move, mock_get, mock_unzip, mock_staging, mock_exists, mock_listdir, mock_rmtree):
+        # rar file without extraction
+        main(['-l', 'https://website/archive.rar', '-d', self.tempdir, '-u', 'user@email.com', '-p', 'testpass123'])
+        mock_move.assert_called_with('/tmp/438cab0f/archive.rar', self.tempdir)
 
-    @mock.patch('rapiddl.os.rmdir')
-    def test_remove_staging(self, mock_rmdir):
-        self.file_handler.remove_staging()
-        mock_rmdir.assert_called_once()
-
+    @mock.patch('rapiddl.shutil.rmtree')
+    @mock.patch('rapiddl.os.listdir', return_value=['archive.rar'])
     @mock.patch('rapiddl.os.path.exists', return_value=True)
-    def test_destination_exists(self, mock_exists):
-        """Tests if the destination path exists."""
-        pass
-        
-    @mock.patch('rapiddl.os.path.exists', return_value=False)
-    def test_destination_does_not_exist(self, mock_exists):
-        """Tests if the destination path does not exists."""
-        path = '/does_not_exist'
-        with self.assertRaises(FileNotFoundError) as exception:
-            self.file_handler.make_staging(path).should_raise()
+    @mock.patch('rapiddl.make_staging', return_value='/tmp/438cab0f')
+    @mock.patch('rapiddl.unzip')
+    @mock.patch('rapiddl.get')
+    @mock.patch('rapiddl.shutil.move')
+    def test_main_method_rar_with_extraction(self, mock_move, mock_get, mock_unzip, mock_staging, mock_exists, mock_listdir, mock_rmtree):
+        # rar file without extraction
+        main(['-l', 'https://website/archive.rar', '-d', self.tempdir, '-u', 'user@email.com', '-p', 'testpass123', '-e'])
+        mock_unzip.assert_called_with(file='/tmp/438cab0f/archive.rar', path=self.tempdir)
 
-    def test_is_compressed_method(self):
-        """Tests if files are compressed via the extension"""
-        zip_file = 'important-documents.zip'
-        result = self.file_handler.is_compressed(zip_file)
-        self.assertEqual(result, True)
+    @mock.patch('rapiddl.shutil.rmtree')
+    @mock.patch('rapiddl.os.listdir', return_value=['video.mp4'])
+    @mock.patch('rapiddl.os.path.exists', return_value=True)
+    @mock.patch('rapiddl.make_staging', return_value='/tmp/438cab0f')
+    @mock.patch('rapiddl.unzip')
+    @mock.patch('rapiddl.get')
+    @mock.patch('rapiddl.shutil.move')
+    def test_main_method_mp4_without_extraction(self, mock_move, mock_get, mock_unzip, mock_staging, mock_exists, mock_listdir, mock_rmtree):
+        # mp4 file without extraction
+        main(['-l', 'https://website/video.mp4', '-d', self.tempdir, '-u', 'user@email.com', '-p', 'testpass123'])
+        mock_move.assert_called_with('/tmp/438cab0f/video.mp4', self.tempdir)
 
-        txt_file = 'other-document.txt'
-        result = self.file_handler.is_compressed(txt_file)
-        self.assertEqual(result, False)
 
-
-if __name__ == '__main__':
-    unittest.main()
